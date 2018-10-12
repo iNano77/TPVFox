bae821017f7345d8a045f3d5e9be090774e2a03a
alagoroVigo <alberto@alagoro.com>
2018-10-13 00:43:01 +0200

    actualizacion a master 0042a

--- BD/tpvfox_limpia_2018 0.0.42a.sql
+++ BD/tpvfox_limpia_2018 0.0.42a.sql
@@ -2022,3 +2022,7 @@ DROP TABLE IF EXISTS `modulo_balanza`;
 CREATE TABLE `modulo_balanza` ( `idBalanza` INT NOT NULL AUTO_INCREMENT , `nombreBalanza` VARCHAR(100) NOT NULL , `modelo` VARCHAR(100) NOT NULL , `conTecla` VARCHAR(3) NOT NULL , PRIMARY KEY (`idBalanza`)) ENGINE = InnoDB;
 DROP TABLE IF EXISTS `modulo_balanza_plus`;
 CREATE TABLE `modulo_balanza_plus` ( `idBalanza` INT NOT NULL , `plu` INT(10) NOT NULL , `tecla` INT(100) NOT NULL , `idArticulo` INT NOT NULL ) ENGINE = InnoDB;
+
+
+DELETE FROM `familiasTienda` WHERE `idTienda`=1;
+ALTER TABLE `familiasTienda` ADD `id` INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`id`);
